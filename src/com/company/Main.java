ededaf2693a91c5dd12552ac539f333a6fc96398
katrich1996 <sergey.katrich96@mail.ru>
2022-04-02 13:04:42 +0300

    Test

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,18 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        int i = 0;
+        while (i < 10) {
+            i = i + 1;
+            System.out.print(i + " ");
+        }
+        System.out.println("");
+        for (int x = 10; x > 0; x--) {
+            System.out.print(x + " ");
+
+
+        }
     }
 }
+
+
